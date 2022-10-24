cbd73461e12cc05e60cab2a8c5cba10994ec8b92
kasdan <kasdan23@gmail.com>
2022-10-24 20:31:46 +0600

    first commit

---
+++ src/main/java/kg/pitak/pitak/PitakApplication.java
@@ -0,0 +1,13 @@
+package kg.pitak.pitak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PitakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PitakApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/pitak/pitak/PitakApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.pitak.pitak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PitakApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
